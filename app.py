c644c122f67f5c960e27da460bf8bab47a026437
Rodrigo Attique Santana <rodrigo.attique@sp.senai.br>
2023-08-04 13:12:51 -0300

    first commit

--- app.py
+++ app.py
@@ -7,10 +7,17 @@ def create_app(): # cria uma função para definir o aplicativo
     def index(): # função que gerencia rota
         return render_template("index.html") # combina o python com html
 
+    @app.route("/alunos")
+    def alunos():
+        import json
+        from database.dados import alunos
+        return alunos
+
     @app.route("/login")
     def login():
         return "<H1>Login ainda não implementado</h1>"
+    
     return app # retorna o app criado
 
 if __name__ == "__main__": # 'função principal' do python
-    create_app().run() # executa o flask na porta http://127.0.0.1:5000
+    create_app().run(debug=True) # executa o flask na porta http://127.0.0.1:5000
