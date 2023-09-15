ed6fbf154cbbc39c08d5a78d78eb53ab11b59c35
Rodrigo Attique Santana <rodrigo.attique@sp.senai.br>
2023-09-15 09:06:42 -0300

    login form e template

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template # importa bibliotecas
+from flask import Flask, redirect, render_template, request, session, url_for
 
 def create_app(): # cria uma função para definir o aplicativo
     app = Flask(__name__) # instancia o Flask
-    
+    app.secret_key = "abax"
     @app.route("/") # cria uma rota
     def index(): # função que gerencia rota
         nome = "Rodrigo 123"
@@ -20,9 +20,22 @@ def create_app(): # cria uma função para definir o aplicativo
         
         return render_template("lista.html", alunos=alunos, media=media )
 
-    @app.route("/login")
+    @app.route("/login", methods=('POST', 'GET'))
     def login():
-        return render_template("login.html")
+        error = None
+        if request.method == 'POST':
+            email = request.form.get('email')
+            senha = request.form.get('senha')
+
+            from database.dados import alunos
+            for k,v in alunos.items():
+                if email == v.get('usuario') and senha == v.get('senha'):
+                    session['user'] = v
+                    return redirect(url_for('index'))
+                else:
+                    error = "Usuario ou senha inválidos!"
+
+        return render_template("login.html", error=error)
     
     return app # retorna o app criado
 
