344154bb04a658e11781aabfa525286d31fffe54
unknown <usuario@ESS185N1264916.sp.local>
2023-09-22 14:42:54 -0300

    perfil de usuario pre implementado

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request, session, url_for
+from flask import Flask, redirect, render_template, request, session, url_for, flash
 
 def create_app(): # cria uma função para definir o aplicativo
     app = Flask(__name__) # instancia o Flask
@@ -36,7 +36,29 @@ def create_app(): # cria uma função para definir o aplicativo
                     error = "Usuario ou senha inválidos!"
 
         return render_template("login.html", error=error)
-    
+
+    @app.route("/logout")
+    def logout():
+        session.clear()
+        return redirect(url_for("index"))
+
+    @app.route("/perfil")
+    def perfil():
+        if 'user' not in session:
+            flash("Usuário não está logado!")
+            return redirect(url_for("login"))
+        
+        if request.method == "POST": # lógica para salvar
+            pass
+        
+        # senão pega usuario no banco
+        from database.dados import alunos
+        for k,v in alunos.items():
+            if v.get("usuario") == session['user'].get("usuario"):
+                usuario = v # {"nome": "Batman", "t": 9.1, "p1": 8.5, "p2": 9, "avatar": url_for('static', filename="images/batman.jpg"), "usuario":"batman@email.com", "senha":"curinga"}
+        
+        return render_template("perfil.html", usuario=usuario)
+
     return app # retorna o app criado
 
 if __name__ == "__main__": # 'função principal' do python
--- database/dados.py
+++ database/dados.py
@@ -1,10 +1,10 @@
 from flask.helpers import url_for
 alunos = {
-10: {"nome": "Batman", "t": 9.1, "p1": 8.5, "p2": 9, "avatar": url_for('static', filename="images/batman.jpg"), "usuario":"batman", "senha":"curinga"}
-,11: {"nome": "Robin", "t": 10, "p1": 9.5, "p2": 10, "avatar": url_for('static', filename="images/robin.jpg"), "usuario":"robin", "senha":"slade"}
-,12: {"nome": "Volverine", "t": 6, "p1": 7, "p2": 8, "avatar": url_for('static', filename="images/volverine.png"), "usuario":"volverine", "senha":"xavier"}
-,13: {"nome": "Gibak", "t": 8, "p1": 9.5, "p2": 10, "avatar": url_for('static', filename="images/gibak.jpg"), "usuario":"gibak", "senha":"bucky"}
-,14: {"nome": "Barbie", "t": 3, "p1": 9.5, "p2": 7, "avatar": url_for('static', filename="images/barbie.jpg"), "usuario":"barbie", "senha":"ken"}
-,15: {"nome": "zaza", "t": 4, "p1": 9.5, "p2": 7, "avatar": url_for('static', filename="images/zaza.jpg"), "usuario":"zaza", "senha":"cocorico"}
-,15: {"nome": "saitama", "t": 3, "p1": 9.5, "p2": 7, "avatar": url_for('static', filename="images/saitama.jpg"), "usuario":"saitama", "senha":"saitama"}
+10: {"nome": "Batman", "t": 9.1, "p1": 8.5, "p2": 9, "avatar": url_for('static', filename="images/batman.jpg"), "usuario":"batman@email.com", "senha":"curinga"}
+,11: {"nome": "Robin", "t": 10, "p1": 9.5, "p2": 10, "avatar": url_for('static', filename="images/robin.jpg"), "usuario":"robin@email.com", "senha":"slade"}
+,12: {"nome": "Volverine", "t": 6, "p1": 7, "p2": 8, "avatar": url_for('static', filename="images/volverine.png"), "usuario":"volverine@email.com", "senha":"xavier"}
+,13: {"nome": "Gibak", "t": 8, "p1": 9.5, "p2": 10, "avatar": url_for('static', filename="images/gibak.jpg"), "usuario":"gibak@email.com", "senha":"bucky"}
+,14: {"nome": "Barbie", "t": 3, "p1": 9.5, "p2": 7, "avatar": url_for('static', filename="images/barbie.jpg"), "usuario":"barbie@email.com", "senha":"ken"}
+,15: {"nome": "zaza", "t": 4, "p1": 9.5, "p2": 7, "avatar": url_for('static', filename="images/zaza.jpg"), "usuario":"zaza@email.com", "senha":"cocorico"}
+,15: {"nome": "saitama", "t": 3, "p1": 9.5, "p2": 7, "avatar": url_for('static', filename="images/saitama.jpg"), "usuario":"saitama@email.com", "senha":"saitama"}
 }
