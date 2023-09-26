d493e705c0317c3d74cf2a639eb7e0614cd09eb5
unknown <usuario@ESS185N1264916.sp.local>
2023-09-26 15:23:07 -0300

    Edit ok

--- alunos.py
+++ alunos.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, render_template, request, url_for
+from flask import Blueprint, redirect, render_template, request, url_for, flash
 
 bp = Blueprint("Aluno", __name__)
 
@@ -29,6 +29,36 @@ def delete(id):
 
     return render_template("alunos/delete.html", id=id, aluno=aluno)
 
-@bp.route("/alunos/<int:id>/edit")
+@bp.route("/alunos/<int:id>/edit", methods=("GET", "POST"))
 def edit(id):
-    return render_template("alunos/form.html")
+    from database.dados import alunos
+    erros = []
+    aluno = alunos.get(id)
+    
+    if request.method=="POST":
+        nome = request.form.get("nome")
+        usuario = request.form.get("email")
+        t = float(request.form.get("t"))
+        p1 = float(request.form.get("p1"))
+        p2 = float(request.form.get("p2"))
+        
+        # Validações
+        if not nome: erros.append("Nome é um campo obrigatório")
+        if not usuario: erros.append("Email é um campo obrigatório")
+        if not t or t <0 or t > 10: erros.append("Trabalho é um campo obrigatório ou valores não entre 0-10")
+        if not p1 or p1 <0 or p1 > 10: erros.append("Prova 1 é um campo obrigatório ou valores não entre 0-10")
+        if not p2 or p2 <0 or p2 > 10: erros.append("Prova 2 é um campo obrigatório ou valores não entre 0-10")
+
+        if len(erros) == 0:
+            # altera usuário no banco de dados
+            alunos[id]["nome"] = nome
+            alunos[id]["usuario"] = usuario
+            alunos[id]["t"] = t
+            alunos[id]["p1"] = p1
+            alunos[id]["p2"] = p2
+            
+            flash(f"Usuário {nome}, salvo com sucesso!")
+
+            return redirect(url_for("Aluno.edit", id=id))
+
+    return render_template("alunos/form.html", id=id, aluno=aluno, erros=erros)
