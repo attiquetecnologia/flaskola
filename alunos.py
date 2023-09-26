92ba86095cef061ebc1d0f9c191abad1aae39531
unknown <usuario@ESS185N1264916.sp.local>
2023-09-26 10:51:36 -0300

    lista de alunos com botões

--- alunos.py
+++ alunos.py
@@ -4,13 +4,20 @@ bp = Blueprint("Aluno", __name__)
 
 @bp.route("/alunos/lista")
 def lista():
-    return render_template("alunos/lista.html")
+    from database.dados import alunos
+
+    # Função lambda cria funções de 1 linha só
+    # media = lambda t,p1,p2: t*.3+p1*.35+p2*.35
+    def media(t, p1, p2):
+        return t*.3+p1*.35+p2*.35
+
+    return render_template("alunos/lista.html", alunos=alunos, media=media)
 
 @bp.route("/alunos/add")
 def add():
     return render_template("alunos/form.html")
 
-@bp.route("/alunos/<int:id>delete")
+@bp.route("/alunos/<int:id>/delete")
 def delete(id):
     return render_template("alunos/delete.html")
 
