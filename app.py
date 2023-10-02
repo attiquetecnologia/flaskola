56ae17456aed67d5d600142a1840968d8577e12a
unknown <usuario@ESS185N1264916.sp.local>
2023-10-02 15:03:01 -0300

    conexão com banco ok

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-from flask import Flask, redirect, render_template, request, session, url_for, flash
+import click
+from flask import Flask, render_template
+from flask.cli import with_appcontext
+from database.connection import db
 
 def create_app(): # cria uma função para definir o aplicativo
     app = Flask(__name__) # instancia o Flask
     app.secret_key = "abax"
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqldb://root:5e5i\@123@localhost:3306/projeto"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    db.init_app(app)
+    app.cli.add_command(init_db_command)
+
     @app.route("/") # cria uma rota
     def index(): # função que gerencia rota
         nome = "Rodrigo 123"
@@ -18,6 +28,21 @@ def create_app(): # cria uma função para definir o aplicativo
     app.register_blueprint(bp)
 
     return app # retorna o app criado
+
+def init_db():
+    db.drop_all()
+    # db.create_all()
+    db.reflect()
+
+
+@click.command("init-db")
+@with_appcontext
+def init_db_command():
+    """Clear existing data and create new tables."""
+    
+    init_db()
+    click.echo("Initialized the database.")
+    
 
 if __name__ == "__main__": # 'função principal' do python
     create_app().run(debug=True) # executa o flask na porta http://127.0.0.1:5000
