da4de1600aa2415fd99874eb7f8bcc502b335738
unknown <usuario@ESS185N1264916.sp.local>
2023-10-03 10:55:15 -0300

    Adicionar novos alunos funcionando

--- alunos/controller.py
+++ alunos/controller.py
@@ -1,53 +1,49 @@
 from flask import Blueprint, redirect, render_template, request, url_for, flash
-from sqlalchemy import text
+from sqlalchemy import select
 from database.connection import db
+from .model import Aluno
 
 bp = Blueprint("Aluno", __name__)
 
 @bp.route("/alunos/lista")
 def lista():
-    from database.dados import alunos
-    lista = db.session.execute(text("select * from alunos"))
+    lista = db.session.scalars(select(Aluno))
 
     # Função lambda cria funções de 1 linha só
     # media = lambda t,p1,p2: t*.3+p1*.35+p2*.35
     def media(t, p1, p2):
         return t*.3+p1*.35+p2*.35
 
-    return render_template("alunos/lista.html", lista=lista, alunos=alunos, media=media)
+    return render_template("alunos/lista.html", lista=lista, media=media)
 
 @bp.route("/alunos/add", methods=("GET", "POST"))
 def add():
-    from database.dados import alunos
     erros = []
     
     if request.method=="POST":
         nome = request.form.get("nome")
-        usuario = request.form.get("email")
+        email = request.form.get("email")
         t = float(request.form.get("t"))
         p1 = float(request.form.get("p1"))
         p2 = float(request.form.get("p2"))
         
         # Validações
         if not nome: erros.append("Nome é um campo obrigatório")
-        if not usuario: erros.append("Email é um campo obrigatório")
+        if not email: erros.append("Email é um campo obrigatório")
         if not t or t <0 or t > 10: erros.append("Trabalho é um campo obrigatório ou valores não entre 0-10")
         if not p1 or p1 <0 or p1 > 10: erros.append("Prova 1 é um campo obrigatório ou valores não entre 0-10")
         if not p2 or p2 <0 or p2 > 10: erros.append("Prova 2 é um campo obrigatório ou valores não entre 0-10")
 
-        #verifica se já existe
-        if usuario in str(alunos): erros.append("Email já registrado")
-
         if len(erros) == 0:
-            last_id = max(alunos.keys()) # ultimo id
-            id = last_id+1
             # salva usuário no banco de dados
-            alunos[id] = {"nome": nome, "usuario": usuario, "t": t
-            , "p1": p1, "p2": p2 }
-            
+            aluno = Aluno(**{"nome": nome, "email": email, "senha": '123', "t": t
+            , "p1": p1, "p2": p2 })
+            db.session.add(aluno)
+            db.session.commit() # persiste no banco
+
             flash(f"Usuário {nome}, salvo com sucesso!")
 
-            return redirect(url_for("Aluno.edit", id=id))
+            return redirect(url_for("Aluno.edit", id=aluno.id))
 
     return render_template("alunos/form.html", erros=erros)
 
