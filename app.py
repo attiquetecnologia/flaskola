164bdcc71b0373f039e79c25bc2cd8c3521b8c62
MarianyPorcini <marianyporcini@gmail.com>
2025-05-20 15:05:12 -0300

    Mariany teste para index exibir atividades e livros

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import click
-from flask import Flask, render_template, request, flash, redirect
+from flask import Flask, render_template
 from flask.cli import with_appcontext
 from sqlalchemy import text
 from database.connection import db
 
+# IMPORTAR O MODELO DE ATIVIDADE
+from atividades.model import Atividade
+from livros.model import Livro
+
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'txt', 'docx', 'doc'}
-
-
 
 def create_app():
     app = Flask(__name__)
@@ -18,14 +20,15 @@ def create_app():
 
     db.init_app(app)
     app.cli.add_command(init_db_command)
-    
 
+    # ROTA INDEX MOSTRANDO LISTA DE ATIVIDADES
     @app.route("/")
     def index():
-        nome = "Rodrigo 123"
-        return render_template("index.html", nome=nome)
+        livros = Livro.query.all()
+        atividades = Atividade.query.all()
+        return render_template("index.html", livros=livros, atividades=atividades)
 
-    # Registrar blueprints
+    # REGISTRAR BLUEPRINTS
     from componentes import bp
     app.register_blueprint(bp)
 
@@ -35,13 +38,13 @@ def create_app():
     from alunos.controller import bp
     app.register_blueprint(bp)
 
-    from livros.controller import bp  # <-- caminho atualizado
+    from livros.controller import bp
     app.register_blueprint(bp)
 
     from atividades.controller import bp
     app.register_blueprint(bp)
 
-    return app # retorna o app criado
+    return app
 
 def init_db():
     db.drop_all()
